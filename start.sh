5f053ff2c9f31916a12c143ba9d3549ecb242509
Zhongfu Li <me@znx.cc>
2021-09-11 23:16:22 +0800

    fix: start script name change

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-screen -dmS tg.sgdefi sh -c 'echo "ctrl+a d to detach"; while true; do python3.8 -m app; sleep 300; done'
+screen -dmS tg.scamofbot sh -c 'echo "ctrl+a d to detach"; while true; do python3.8 -m app; sleep 300; done'
