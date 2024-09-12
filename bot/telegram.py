948b9ce375493fc08d195903b6aa22967e7ffe44
zhongfu <me@zhongfu.li>
2024-09-12 21:28:29 +0800

    fix: retry connection to telegram servers forever

--- bot/telegram.py
+++ bot/telegram.py
@@ -1,7 +1,14 @@
 from telethon import TelegramClient, events
 from config import TG_SESSION, TG_API_ID, TG_API_HASH, TG_API_TOKEN
 
-client = TelegramClient(TG_SESSION, TG_API_ID, TG_API_HASH)
+client = TelegramClient(
+    TG_SESSION,
+    TG_API_ID,
+    TG_API_HASH,
+    connection_retries=None,
+    retry_delay=10,
+    auto_reconnect=True,
+)
 client.parse_mode = 'html'
 
 async def tg_start():
